c93bba8064f25083269481864f58d0dc4b9128c6
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-05-07 12:12:07 +0300

    load dataset for model training

---
+++ TrainingModel.R
@@ -0,0 +1,20 @@
+# Load dataset
+crop_data <- read.csv("data/yield_df.csv", colClasses = c(
+  id = "integer",
+  Area = "factor",
+  Item = "factor",
+  Year = "integer",
+  area_yield = "integer",
+  average_rain_fall_mm_per_year = "integer",
+  pesticides_tonnes = "numeric",
+  avg_temp = "numeric"
+))
+
+# Display the structure of the dataset
+str(crop_data)
+
+# View the first few rows of the dataset
+head(crop_data)
+
+# View the dataset in a separate viewer window
+View(crop_data)
